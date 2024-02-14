568926b41ff00d9d26d564b4be8c5361040aa0a0
Megan Yu <yu.megan@gmail.com>
2024-02-14 14:36:40 -0500

    email and password text fields - mui imports - working in App.tsx until routing finished

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,7 +1,42 @@
 import "./App.css";
+import * as React from 'react';
+import { FormControl, IconButton, InputAdornment, InputLabel, OutlinedInput, TextField } from "@mui/material";
+import {Visibility, VisibilityOff} from '@mui/icons-material';
 
 function App() {
-  return <>App</>;
+
+  const [showPassword, setShowPassword] = React.useState(false);
+
+  const handleClickShowPassword = () => setShowPassword((show) => !show);
+
+  const handleMouseDownPassword = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+  };
+
+  return <>
+    <TextField id="outlined-basic" label="Email" variant="outlined" />
+    <FormControl sx={{ m: 1, width: '25ch' }} variant="outlined">
+      <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
+      <OutlinedInput
+        id="outlined-adornment-password"
+        type={showPassword ? 'text' : 'password'}
+        endAdornment={
+          <InputAdornment position="end">
+            <IconButton
+              aria-label="toggle password visibility"
+              onClick={handleClickShowPassword}
+              onMouseDown={handleMouseDownPassword}
+              edge="end"
+            >
+              {showPassword ? <VisibilityOff /> : <Visibility />}
+            </IconButton>
+          </InputAdornment>
+        }
+        label="Password"
+      />
+    </FormControl>
+
+  </>;
 }
 
 export default App;
