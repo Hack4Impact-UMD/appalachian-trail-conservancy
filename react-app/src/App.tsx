14702a1e421408f620a1a90b6398050f58781526
nick farber <nick@home>
2024-02-20 13:52:00 -0500

    Fixed pages and migrated router

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,8 +1,25 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import {BrowserRouter, Route, Routes } from "react-router-dom";
+import homePage from "./pages/HomePage"
+import pathwayPage from "./pages/PathwayPage"
+import quizPage from "./pages/QuizPage"
+import trainingPage from "./pages/TrainingPage"
 import { ThemeProvider } from "@mui/material";
 import theme from "./muiTheme.ts";
 
 function App() {
-  return <ThemeProvider theme={theme}>App</ThemeProvider>;
+   return <ThemeProvider theme={theme}>
+    <BrowserRouter>
+      <Routes>
+        <Route path = "/pages/HomePage" element = {<homePage/>}/>
+        <Route path = "/pages/PathwayPage" element = {<pathwayPage/>}/>
+        <Route path = "/pages/QuizPage" element = {<quizPage/>}/>
+        <Route path = "/pages/TrainingPage" element = {<trainingPage/>}/>
+    </Routes>
+  </BrowserRouter>
+ </ThemeProvider>;
 }
 
 export default App;
--- react-app/src/main.tsx
+++ react-app/src/main.tsx
@@ -2,22 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import {BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import homePage from "./pages/homePage"
-import pathwayPage from "./pages/pathwayPage"
-import quizPage from "./pages/quizPage"
-import trainingPage from "./pages/trainingPage"
 
-ReactDOM.render(
-	<React.StrictMode>
-		<Router>
-			<Routes>
-				<Route path = "/pages/homePage" element = {<homePage/>}/>
-				<Route path = "/pages/pathwayPage" element = {<pathwayPage/>}/>
-				<Route path = "/pages/quizPage" element = {<quizPage/>}/>
-				<Route path = "/pages/trainingPage" element = {<trainingPage/>}/>
-			</Routes>
-		</Router>
-	</React.StrinctMode>
-
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
