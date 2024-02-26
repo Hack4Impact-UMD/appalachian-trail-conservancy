b4f88963a2e1eed18d42ec34ee4563931c786b2f
Margaret Kato <margaret.tkato@gmail.com>
2024-02-26 08:23:19 -0500

    added trainings in progress page in a new folder under pages. layout is complete but there are placeholders for the profile picture and nav bar.

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -4,6 +4,7 @@ import HomePage from "./pages/HomePage/HomePage.tsx";
 import PathwayPage from "./pages/PathwayPage/PathwayPage.tsx";
 import QuizPage from "./pages/QuizPage/QuizPage.tsx";
 import TrainingPage from "./pages/TrainingPage/TrainingPage.tsx";
+import TrainingsInProgressPage from "./pages/TrainingsInProgressPage/TrainingsInProgressPage.tsx";
 import LoginPage from "./pages/LoginPage/LoginPage.tsx";
 import { ThemeProvider } from "@mui/material";
 import theme from "./muiTheme.ts";
@@ -17,6 +18,7 @@ function App() {
           <Route path="/pathway" element={<PathwayPage />} />
           <Route path="/quiz" element={<QuizPage />} />
           <Route path="/training" element={<TrainingPage />} />
+          <Route path="/trainingsInProgress" element={<TrainingsInProgressPage />} />
           <Route path="/login" element={<LoginPage />} />
         </Routes>
       </BrowserRouter>
