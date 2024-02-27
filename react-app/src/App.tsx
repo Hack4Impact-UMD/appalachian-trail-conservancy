0844c3d03f998c90ed462565bd3bd2c40df91de2
Akash Patil <110560351+akashpatil0@users.noreply.github.com>
2024-02-26 19:55:54 -0500

    Reformated all pages with prettier. Changed titles of Training pages, and updated component imports.

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,13 +1,10 @@
 import "./index.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage/HomePage.tsx";
-import PathwayPage from "./pages/PathwayPage/PathwayPage.tsx";
-import QuizPage from "./pages/QuizPage/QuizPage.tsx";
-import TrainingPage from "./pages/TrainingPage/TrainingPage.tsx";
 import TrainingsInProgressPage from "./pages/TrainingsInProgressPage/TrainingsInProgressPage.tsx";
 import LoginPage from "./pages/LoginPage/LoginPage.tsx";
 import Dashboard from "./pages/DashboardPage/Dashboard.tsx";
-import TrainingCompletedPage from "./pages/TrainingCompletedPage/TrainingCompletedPage.tsx";
+import TrainingsCompletedPage from "./pages/TrainingsCompletedPage/TrainingsCompletedPage.tsx";
 import { ThemeProvider } from "@mui/material";
 import theme from "./muiTheme.ts";
 
@@ -17,13 +14,16 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<HomePage />} />
-          <Route path="/pathway" element={<PathwayPage />} />
-          <Route path="/quiz" element={<QuizPage />} />
-          <Route path="/training" element={<TrainingPage />} />
-          <Route path="/trainingsInProgress" element={<TrainingsInProgressPage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/trainingCompleted" element={<TrainingCompletedPage/>}/>
+          <Route
+            path="/trainingsInProgress"
+            element={<TrainingsInProgressPage />}
+          />
+          <Route
+            path="/trainingsCompleted"
+            element={<TrainingsCompletedPage />}
+          />
         </Routes>
       </BrowserRouter>
     </ThemeProvider>
