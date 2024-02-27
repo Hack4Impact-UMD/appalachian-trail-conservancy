65a84e2ff05d043564d25a9446069deac0fb892b
Akash Patil <110560351+akashpatil0@users.noreply.github.com>
2024-02-26 20:02:50 -0500

    Removed home page, updated root route to dashboard page
    Co-authored-by: sophtsai <sophietsai31@gmail.com>

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,9 +1,8 @@
 import "./index.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import HomePage from "./pages/HomePage/HomePage.tsx";
 import TrainingsInProgressPage from "./pages/TrainingsInProgressPage/TrainingsInProgressPage.tsx";
 import LoginPage from "./pages/LoginPage/LoginPage.tsx";
-import Dashboard from "./pages/DashboardPage/Dashboard.tsx";
+import Dashboard from "./pages/DashboardPage/DashboardPage.tsx";
 import TrainingsCompletedPage from "./pages/TrainingsCompletedPage/TrainingsCompletedPage.tsx";
 import { ThemeProvider } from "@mui/material";
 import theme from "./muiTheme.ts";
@@ -13,9 +12,8 @@ function App() {
     <ThemeProvider theme={theme}>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<HomePage />} />
+          <Route path="/" element={<Dashboard />} />
           <Route path="/login" element={<LoginPage />} />
-          <Route path="/dashboard" element={<Dashboard />} />
           <Route
             path="/trainingsInProgress"
             element={<TrainingsInProgressPage />}
