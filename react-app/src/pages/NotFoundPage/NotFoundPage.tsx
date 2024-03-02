031a95f1b3d65a7e55e5e86e55affd2a42f3b8b6
Megan Yu <yu.megan@gmail.com>
2024-03-02 09:57:21 -0500

    put elements on page - all left aligned, need to style

--- react-app/src/pages/NotFoundPage/NotFoundPage.tsx
+++ react-app/src/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,7 +1,15 @@
-// import * as React from "react";
+import styles from "./NotFoundPage.module.css";
+import primaryLogo from "../../assets/atc-primary-logo.png";
+import {Button} from "@mui/material";
 
 function NotFoundPage () {
-    return (<><h1>not found</h1></>)
+    return (<>
+        <img src={primaryLogo} className={styles.logoImg} />
+        <h1>404</h1>
+        <h2>Page not found.</h2>
+        <p>The page you are looking for is unavailable or missing.</p>
+        <Button>Go back to dashboard</Button>
+    </>)
 }
 
 export default NotFoundPage
