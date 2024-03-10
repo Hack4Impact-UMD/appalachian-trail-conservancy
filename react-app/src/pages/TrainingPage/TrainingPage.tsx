552a6bfc5ddf9c588b78f7254b2b5185adf780b3
nick farber <nick@home>
2024-03-09 20:54:44 -0500

    Fixed formatting and cleaned up CSS

--- react-app/src/pages/TrainingPage/TrainingPage.tsx
+++ react-app/src/pages/TrainingPage/TrainingPage.tsx
@@ -1,16 +1,11 @@
 import styles from "./TrainingPage.module.css";
 import TrainingCard from "../../components/TrainingCard/Training.tsx";
-import Box from '@mui/material/Box';
-import Stepper from '@mui/material/Stepper';
-import Step from '@mui/material/Step';
-import StepLabel from '@mui/material/StepLabel';
+import Box from "@mui/material/Box";
+import Stepper from "@mui/material/Stepper";
+import Step from "@mui/material/Step";
+import StepLabel from "@mui/material/StepLabel";
 
-const steps = [
-  '',
-  '',
-  '',
-  ''
-];
+const steps = ["", "", "", ""];
 
 function TrainingPage() {
   return (
@@ -23,17 +18,18 @@ function TrainingPage() {
           <div className={styles.imgContainer}>
             <img src="https://pyxis.nymag.com/v1/imgs/7aa/21a/c1de2c521f1519c6933fcf0d08e0a26fef-27-spongebob-squarepants.rsquare.w400.jpg" />
           </div>
-          <Box sx={{ width: '100%' }}>
-      <Stepper activeStep={0} alternativeLabel>
-        {steps.map((label) => (
-          <Step key={label}>
-            <StepLabel>{label}</StepLabel>
-          </Step>
-        ))}
-      </Stepper>
-    </Box>
+          <div className={styles.stepContainer}>
+            <Box sx={{ width: "100%" }}>
+              <Stepper activeStep={0} alternativeLabel>
+                {steps.map((label) => (
+                  <Step key={label}>
+                    <StepLabel>{label}</StepLabel>
+                  </Step>
+                ))}
+              </Stepper>
+            </Box>
+          </div>
         </div>
-        
       </div>
     </>
   );
