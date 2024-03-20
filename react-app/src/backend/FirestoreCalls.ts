70db696f38f453909c19d2bcc8a8901b5673442f
Akash Patil <110560351+akashpatil0@users.noreply.github.com>
2024-03-20 15:53:23 -0400

    added getVolunteerWithAuth function, should be used in combination with an user context provider

--- react-app/src/backend/FirestoreCalls.ts
+++ react-app/src/backend/FirestoreCalls.ts
@@ -4,6 +4,7 @@ import {
   addDoc,
   deleteDoc,
   getDoc,
+  getDocs,
   updateDoc,
   runTransaction,
   query,
@@ -13,6 +14,29 @@ import { db } from "../config/firebase";
 import { Volunteer, VolunteerID } from "../types/UserType";
 import { Training, TrainingID } from "../types/TrainingType";
 import { Pathway, PathwayID } from "../types/PathwayType";
+
+/* These can be edited depending on collection name and types */
+
+export function getVolunteerWithAuth(auth_id: string): Promise<VolunteerID> {
+  return new Promise((resolve, reject) => {
+    const volunteersRef = query(
+      collection(db, "Users"),
+      where("auth_id", "==", auth_id)
+    );
+    getDocs(volunteersRef)
+      .then((volunteerSnapshot) => {
+        if (volunteerSnapshot.size > 0) {
+          const volunteerData = volunteerSnapshot.docs[0].data() as Volunteer;
+          resolve({ ...volunteerData, id: volunteerSnapshot.docs[0].id });
+        } else {
+          reject(new Error("Volunteer does not exist"));
+        }
+      })
+      .catch((e) => {
+        reject(e);
+      });
+  });
+}
 
 export function addTraining(training: Training): Promise<string> {
   return new Promise((resolve, reject) => {
