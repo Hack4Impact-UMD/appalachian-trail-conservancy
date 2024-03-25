c9d08e4b493c78f0c0ee8df8043cff61d98061e3
Brandon <bnewman6@terpmail.umd.edu>
2024-03-25 15:01:23 -0400

    added pathwayCard, updated trainingCard, Dashboard

--- react-app/src/components/PathwayCard/Pathway.tsx
+++ react-app/src/components/PathwayCard/Pathway.tsx
@@ -1,51 +1,51 @@
 import React from "react";
-import LinearProgressWithLabel from "./LinearProgressWithLabel";
+import LinearProgressWithLabel from "./LinearProgressWithLabelPathway";
 
-import styles from "./Training.module.css";
+import styles from "./Pathway.module.css";
 
-interface TrainingCardProps {
+interface PathwayCardProps {
   image: string;
   title: string;
   progress?: number; // Optional progress value
 }
 
-const TrainingCard: React.FC<TrainingCardProps> = ({
+const PathwayCard: React.FC<PathwayCardProps> = ({
   image,
   title,
   progress,
 }) => {
   const renderMarker = () => {
     if (progress === undefined) {
-      // Training not started
+      // Pathway not started
       return (
         <div className={styles.marker} style={{ borderColor: "var(--blue-gray)", color: "var(--blue-gray)" }}>
           NOT STARTED
         </div>
       );
     } else if (progress === 100) {
-      // Training completed
+      // Pathway completed
       return (
         <div className={styles.marker} style={{ backgroundColor: "var(--blue-gray)", color: "white" }}>
           COMPLETED
         </div>
       );
     } else {
-      // Training in progress
+      // Pathway in progress
       return <LinearProgressWithLabel value={progress} />;
     }
   };
 
   return (
-    <div className={styles["trainingCard"]}>
-      <div className={styles["trainingImage"]}>
-        <img src={image} alt="Training Task" />
+    <div className={styles["pathwayCard"]}>
+      <div className={styles["pathwayImage"]}>
+        <img src={image} alt="Pathway Task" />
       </div>
-      <div className={styles["trainingContent"]}>
-        <div className={styles["trainingTitle"]}>{title}</div>
+      <div className={styles["pathwayContent"]}>
+        <div className={styles["pathwayTitle"]}>{title}</div>
         <div className={styles["progressBar"]}>{renderMarker()}</div>
       </div>
     </div>
   );
 };
 
-export default TrainingCard;
+export default PathwayCard;
