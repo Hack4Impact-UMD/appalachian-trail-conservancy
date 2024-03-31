bedcd41f5732b7ed54e8046f9cff2203ec75d783
nick farber <nick@home>
2024-03-31 13:20:22 -0400

    Early sorting attempt but still not working

--- react-app/src/pages/AchievementsPage/AchievementsPage.tsx
+++ react-app/src/pages/AchievementsPage/AchievementsPage.tsx
@@ -1,12 +1,67 @@
-import { useState } from "react";
 import styles from "./AchievementsPage.module.css";
 import NavigationBar from "../../components/NavigationBar/NavigationBar";
 import { darkGreenButton, whiteEmptyButton } from "../../muiTheme";
-import { Button } from "@mui/material";
-import { MdSort } from "react-icons/md";
+import { FormControl, Select, MenuItem, Button } from "@mui/material";
+import { useState, useEffect } from "react";
+import Certificate from "../../components/CertificateCard/Certificate";
 
 function AchievementsPage() {
-  const [badgesSelected, setBadgesSelected] = useState<boolean>(true); // Initial selected button
+  const months = ["JANUARY", "FEBRUARY", "MARCH", "APRIL", "MAY", 
+  "JUNE", "JULY", "AUGUST", "SEPTEMBER", "OCTOBER", "NOVEMBER", "DECEMBER"];
+
+  const certificates = [
+    {title: "Appalachian",  date: "FEBRUARY 1, 2024"},
+    {title: "Beach", date: "DECEMBER 4, 2023"},
+    {title: "Ocean", date: "AUGUST 17, 2022"},
+    {title: "Savanahh", date: "JANUARY 5, 2020"},
+    {title: "Yosemite", date: "MAY 4, 2023"}
+  ]
+
+  const [badgesSelected, setBadgesSelected] = useState<boolean>(true); 
+  const [sortMode, setSortMode] = useState<string>("newest");
+  const [sortedCards, setSortedCards] = useState< 
+    {title: string; date: string; image: string}[]> ([]);
+
+    const parseDate = (dateString: string) => {
+      const [month, day, year] = dateString.split(" ");
+      const monthIndex = months.indexOf(month.toUpperCase()) + 1;
+      return `${year}-${monthIndex < 10 ? "0" + monthIndex : monthIndex}-${day}`;
+    };
+
+    const sortCards = () => {
+      let sortedCardsCopy = [...sortedCards];
+
+      switch (sortMode) {
+        case "alphabetical":
+          sortedCardsCopy.sort((a, b) => a.title.localeCompare(b.title));
+          break;
+        case "reverse-alphabetical":
+          sortedCardsCopy.sort((a, b) => b.title.localeCompare(a.title));
+          break;
+        case "newest":
+          sortedCardsCopy.sort((a, b) => {
+            const dateA = new Date(parseDate(a.date));
+            const dateB = new Date(parseDate(b.date));
+            return dateB.getTime() - dateA.getTime();
+          });
+          break;
+        case "oldest":
+          sortedCardsCopy.sort((a, b) => {
+            const dateA = new Date(parseDate(a.date));
+            const dateB = new Date(parseDate(b.date));
+            return dateA.getTime() - dateB.getTime();
+          });
+          break;
+      }
+    
+      setSortedCards(sortedCardsCopy);
+    };
+
+  const handleChange = (event: any) => {
+    setSortMode(event.target.value);
+    sortCards();
+
+  };
 
   return (
     <>
@@ -24,24 +79,41 @@ function AchievementsPage() {
             <div className={styles.button}>
               <Button
                 onClick={() => setBadgesSelected(true)}
-                sx={badgesSelected ? darkGreenButton : whiteEmptyButton}>
+                sx={badgesSelected ? darkGreenButton : whiteEmptyButton}
+              >
                 Pathway Badges
               </Button>
             </div>
             <div className={styles.button}>
               <Button
                 onClick={() => setBadgesSelected(false)}
-                sx={!badgesSelected ? darkGreenButton : whiteEmptyButton}>
+                sx={!badgesSelected ? darkGreenButton : whiteEmptyButton}
+              >
                 Training Certificates
               </Button>
             </div>
           </div>
-          <Button sx={whiteEmptyButton}>
-            <MdSort />
-            Sort
-          </Button>
+          <FormControl>
+            <Select
+              value={sortMode}
+              onChange={handleChange}
+            >
+              <MenuItem value={"newest"}>SORT: NEWEST</MenuItem>
+              <MenuItem value={"oldest"}>SORT: OLDEST</MenuItem>
+              <MenuItem value={"alphabetically"}>SORT: A-Z</MenuItem>
+              <MenuItem value={"reverseAlphabetically"}>SORT: Z-A</MenuItem>
+            </Select>
+          </FormControl>
         </div>
-        <div className={styles.cardsContainer}></div>
+        <div className={styles.cardsContainer}>
+        {sortedCards.map((card, index) => (
+          <Certificate
+            image="https://pyxis.nymag.com/v1/imgs/7aa/21a/c1de2c521f1519c6933fcf0d08e0a26fef-27-spongebob-squarepants.rsquare.w400.jpg"
+            title={card.title}
+            date={card.date}
+          />
+          ))}
+        </div>
       </div>
     </>
   );
