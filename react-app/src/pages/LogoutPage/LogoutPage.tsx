83870d5ac679acc71a4649f3cc433e2de5399e89
Rahul-Anantuni <anantuni.rahul@gmail.com>
2024-04-06 17:32:38 -0400

    added css modules

--- react-app/src/pages/LogoutPage/LogoutPage.tsx
+++ react-app/src/pages/LogoutPage/LogoutPage.tsx
@@ -1,18 +1,18 @@
 import React from "react";
 import logo from "../../assets/atc-primary-logo.png";
 import greenCheck from "../../assets/greenCircleCheck.svg";
-import "./LogoutPage.css";
+import styles from "./LogoutPage.module.css";
 import { Link } from "react-router-dom";
 
 const LogoutPage: React.FC = () => {
   return (
-    <div className="centered-container">
-      <img src={logo} alt="Logo" className="logo" />
-      <img src={greenCheck} alt="Green Check" className="checkmark" />
-      <h1 className="logout-text">You have been logged out</h1>
+    <div className={styles["centeredContainer"]}>
+      <img src={logo} alt="Logo" className={styles.logo} />
+      <img src={greenCheck} alt="Green Check" className={styles.checkmark} />
+      <h1 className={styles["logoutText"]}>You have been logged out</h1>
 
       <Link to="/login">
-        <button className="sign-in-button">Sign in Again</button>
+        <button className={styles["signInButton"]}>Sign in Again</button>
       </Link>
     </div>
   );
