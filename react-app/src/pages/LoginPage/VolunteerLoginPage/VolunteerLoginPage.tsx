1bf515d1d6ea77b7d720c0a964c78e33fee6ab60
sophtsai <sophietsai31@gmail.com>
2024-04-08 01:20:11 -0400

    Clean and updates styles for volunteer login

--- react-app/src/pages/LoginPage/VolunteerLoginPage/VolunteerLoginPage.tsx
+++ react-app/src/pages/LoginPage/VolunteerLoginPage/VolunteerLoginPage.tsx
@@ -1,25 +1,14 @@
-import { useState, useCallback, useContext } from "react";
-import {
-  FormControl,
-  IconButton,
-  InputAdornment,
-  InputLabel,
-  OutlinedInput,
-  TextField,
-  Button,
-} from "@mui/material";
+import { useState } from "react";
+import { TextField, Button } from "@mui/material";
+import { forestGreenButton, grayBorderTextField } from "../../../muiTheme";
 import { useNavigate } from "react-router";
-import { Visibility, VisibilityOff } from "@mui/icons-material";
 import { Link, Navigate } from "react-router-dom";
-import { authenticateUser } from "../../../backend/AuthFunctions";
 import { authenticateUserEmailLink } from "../../../backend/AuthFunctions";
 import { getAuth, sendSignInLinkToEmail } from "firebase/auth";
 import { useAuth } from "../../../auth/AuthProvider";
-import { AuthError } from "firebase/auth";
-import Loading from "../../../components/LoadingScreen/Loading";
 import styles from "./VolunteerLoginPage.module.css";
+import Loading from "../../../components/LoadingScreen/Loading";
 import primaryLogo from "../../../assets/atc-primary-logo.png";
-import { forestGreenButton, grayBorderTextField } from "../../../muiTheme";
 import loginBanner from "../../../assets/login-banner.jpeg";
 import app from "../../../config/firebase";
 import greenCheck from "../../../assets/greenCircleCheck.svg";
@@ -33,10 +22,10 @@ function VolunteerLoginPage(history: any) {
   const navigate = useNavigate();
 
   const [showLoading, setShowLoading] = useState<boolean>(false);
-  //Add Error Handling
   const [failureMessage, setFailureMessage] = useState<string>("");
-
   const [email, setEmail] = useState<string>("");
+  const [viewElements, setViewElements] = useState<boolean>(false);
+  const [displayText, setDisplayText] = useState<string>("");
 
   const handleSendLink = async (event: any) => {
     event.preventDefault();
@@ -46,7 +35,9 @@ function VolunteerLoginPage(history: any) {
     if (!pattern.test(email)) {
       setFailureMessage("*Not a valid email");
     } else {
-      console.log(email);
+      setDisplayText(email);
+      setViewElements(true);
+      setFailureMessage("");
       handleLogin();
     }
   };
@@ -60,7 +51,7 @@ function VolunteerLoginPage(history: any) {
     try {
       const auth = getAuth(app);
       await sendSignInLinkToEmail(auth, email, actionCodeSettings);
-      console.log(email);
+      // add error handling if email does not exist
       window.localStorage.setItem("emailForSignIn", email);
     } catch (error) {
       alert(error);
@@ -73,52 +64,13 @@ function VolunteerLoginPage(history: any) {
     return <Navigate to="/" />;
   }
 
-  return (
-    <div className={styles.pageContainer}>
-      <div className={styles.split}>
-        <div className={styles.left}>
-          <img src={loginBanner} alt="Login Image" />
-        </div>
-        <div className={styles.right}>
-          <div className={styles.centered}>
-            <div className={styles.rightImgContainer}>
-              <img src={primaryLogo} alt="ATC Logo" />
-            </div>
-
-            {/* welcome label */}
-            <h1 className={styles.heading}>Welcome!</h1>
-
-            <form
-              className={styles.centered}
-              onSubmit={(event) => {
-                handleSendLink(event);
-              }}
-            >
-              {/* email field */}
-              <div className={styles.alignLeft}>
-                <h3 className={styles.label}>Email</h3>
-              </div>
-              <TextField
-                value={email}
-                sx={{ ...grayBorderTextField, marginBottom: "15px" }}
-                label=""
-                variant="outlined"
-                size="small"
-                onChange={(event) => {
-                  setEmail(event.target.value);
-                }}
-              />
-  const [viewElements, setViewElements] = useState<boolean>(false);
-
-  const [displayText, setDisplayText] = useState<string>("");
-
   const beforeEmail = (
     <div>
       <form
+        className={styles.centered}
         onSubmit={(event) => {
           handleSendLink(event);
-        }}
-      >
+        }}>
         {/* email field */}
         <div className={styles.alignLeft}>
           <h3 className={styles.label}>Email</h3>
@@ -134,58 +86,39 @@ function VolunteerLoginPage(history: any) {
           }}
         />
 
-              {/* send link button */}
-              <Button
-                type="submit"
-                sx={{ ...styledRectButton, ...forestGreenButton }}
-                variant="contained"
-                onClick={(e) => handleSendLink(e)}
-              >
-                {showLoading ? <Loading></Loading> : "Send Link"}
-              </Button>
+        {/* send link button */}
+        <Button
+          type="submit"
+          sx={{ ...styledRectButton, ...forestGreenButton }}
+          variant="contained"
+          onClick={(e) => handleSendLink(e)}>
+          {showLoading ? <Loading></Loading> : "Send Link"}
+        </Button>
 
         {/* error message */}
         <p
           className={
             failureMessage ? styles.showFailureMessage : styles.errorContainer
-          }
-        >
+          }>
           {failureMessage}
         </p>
-      </form>{" "}
+      </form>
     </div>
   );
 
   const sentEmail = (
     <div className={styles.centered}>
-      <div className={styles.greenCheck}>
-        <img src={greenCheck} alt="Green Check" />
-      </div>
-      <h3>Check your email!</h3>
-      <div className={styles.emailText}>
+      <img src={greenCheck} alt="Green Check" height="75" />
+      <h2 className={styles.subheader}>Check your email!</h2>
+      <div className={`${styles.emailText} ${styles.centered}`}>
         <p>
-          {" "}
-          The login link has been sent to{" "}
+          The login link has been sent to&nbsp;
           <span className={styles.greenText}>{displayText}</span>.
         </p>
-        <p> Please use the link to access the dashboard. </p>
+        <p>Please use the link to access your dashboard.</p>
       </div>
     </div>
   );
-
-  const handleSendLink = async (event: any) => {
-    event.preventDefault();
-
-    const pattern: RegExp = /^\S+@\S+$/;
-
-    if (!pattern.test(email)) {
-      setFailureMessage("*Not a valid email");
-    } else {
-      setDisplayText(email);
-      setViewElements(true);
-      setFailureMessage("");
-    }
-  };
 
   return (
     <div className={styles.pageContainer}>
@@ -199,6 +132,8 @@ function VolunteerLoginPage(history: any) {
               <img src={primaryLogo} alt="ATC Logo" />
             </div>
             <h1 className={styles.heading}>Welcome!</h1>
+
+            {/* display check message if valid email is submitted */}
             {viewElements ? sentEmail : beforeEmail}
 
             {/* switch to admin link */}
