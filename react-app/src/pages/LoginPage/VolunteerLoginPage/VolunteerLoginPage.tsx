8373d923ce846cbbe2dc1e0009f64e0477e222ca
nick farber <nick@home>
2024-04-07 20:43:54 -0400

    wierd conflicts

--- react-app/src/pages/LoginPage/VolunteerLoginPage/VolunteerLoginPage.tsx
+++ react-app/src/pages/LoginPage/VolunteerLoginPage/VolunteerLoginPage.tsx
@@ -1,12 +1,24 @@
 import { useState } from "react";
-import { TextField, Button } from "@mui/material";
+import {
+  FormControl,
+  IconButton,
+  InputAdornment,
+  InputLabel,
+  OutlinedInput,
+  TextField,
+  Button,
+} from "@mui/material";
 import { useNavigate } from "react-router";
+import { Visibility, VisibilityOff } from "@mui/icons-material";
 import { Link } from "react-router-dom";
-import { forestGreenButton, grayBorderTextField } from "../../../muiTheme";
-import styles from "./VolunteerLoginPage.module.css";
+import { authenticateUser } from "../../../backend/AuthFunctions";
+import { AuthError } from "firebase/auth";
 import Loading from "../../../components/LoadingScreen/Loading";
+import styles from "./VolunteerLoginPage.module.css";
 import primaryLogo from "../../../assets/atc-primary-logo.png";
+import { forestGreenButton, grayBorderTextField } from "../../../muiTheme";
 import loginBanner from "../../../assets/login-banner.jpeg";
+import greenCheck from "../../../assets/greenCircleCheck.svg";
 
 const styledRectButton = {
   width: 350,
@@ -22,6 +34,71 @@ function VolunteerLoginPage() {
 
   const [email, setEmail] = useState<string>("");
 
+  const [viewElements, setViewElements] = useState<boolean>(false);
+
+  const [displayText, setDisplayText] = useState<string>("");
+
+  const beforeEmail = (
+    <div>
+      <form
+        onSubmit={(event) => {
+          handleSendLink(event);
+        }}
+      >
+        {/* email field */}
+        <div className={styles.alignLeft}>
+          <h3 className={styles.label}>Email</h3>
+        </div>
+        <TextField
+          value={email}
+          sx={{ ...grayBorderTextField, marginBottom: "15px" }}
+          label=""
+          variant="outlined"
+          size="small"
+          onChange={(event) => {
+            setEmail(event.target.value);
+          }}
+        />
+
+        {/* send link button */}
+        <Button
+          type="submit"
+          sx={{ ...styledRectButton, ...forestGreenButton }}
+          variant="contained"
+          onClick={(e) => handleSendLink(e)}
+        >
+          {showLoading ? <Loading></Loading> : "Send Link"}
+        </Button>
+
+        {/* error message */}
+        <p
+          className={
+            failureMessage ? styles.showFailureMessage : styles.errorContainer
+          }
+        >
+          {failureMessage}
+        </p>
+      </form>{" "}
+    </div>
+  );
+
+  const sentEmail = (
+    <div className={styles.centered}>
+      <div className={styles.greenCheck}>
+        <img src={greenCheck} alt="Green Check" />
+      </div>
+      <h3>Check your email!</h3>
+      <div className={styles.emailText}>
+        <p>
+          {" "}
+          The login link has been sent to{" "}
+          <span className={styles.greenText}>{displayText}</span>.
+        </p>
+        <p> Please use the link to access the dashboard. </p>
+      </div>
+    </div>
+  );
+
   const handleSendLink = async (event: any) => {
     event.preventDefault();
 
@@ -30,6 +107,8 @@ function VolunteerLoginPage() {
     if (!pattern.test(email)) {
       setFailureMessage("*Not a valid email");
     } else {
+      setDisplayText(email);
+      setViewElements(true);
       setFailureMessage("");
     }
   };
@@ -45,49 +124,8 @@ function VolunteerLoginPage() {
             <div className={styles.rightImgContainer}>
               <img src={primaryLogo} alt="ATC Logo" />
             </div>
-
-            {/* welcome label */}
             <h1 className={styles.heading}>Welcome!</h1>
-
-            <form
-              className={styles.centered}
-              onSubmit={(event) => {
-                handleSendLink(event);
-              }}>
-              {/* email field */}
-              <div className={styles.alignLeft}>
-                <h3 className={styles.label}>Email</h3>
-              </div>
-              <TextField
-                value={email}
-                sx={{ ...grayBorderTextField, marginBottom: "15px" }}
-                label=""
-                variant="outlined"
-                size="small"
-                onChange={(event) => {
-                  setEmail(event.target.value);
-                }}
-              />
-
-              {/* send link button */}
-              <Button
-                type="submit"
-                sx={{ ...styledRectButton, ...forestGreenButton }}
-                variant="contained"
-                onClick={(e) => handleSendLink(e)}>
-                {showLoading ? <Loading></Loading> : "Send Link"}
-              </Button>
-
-              {/* error message */}
-              <p
-                className={
-                  failureMessage
-                    ? styles.showFailureMessage
-                    : styles.errorContainer
-                }>
-                {failureMessage}
-              </p>
-            </form>
+            {viewElements ? sentEmail : beforeEmail}
 
             {/* switch to admin link */}
             <Link to="/login/admin">
