8140049f59156f60868774034a6ee773b55b7144
sophtsai <sophietsai31@gmail.com>
2024-04-08 11:58:04 -0400

    Update footer logo and links, open links in new tab

--- react-app/src/components/Footer/Footer.tsx
+++ react-app/src/components/Footer/Footer.tsx
@@ -1,14 +1,12 @@
 import styles from "./Footer.module.css";
 import { FaFacebookF, FaTiktok, FaYoutube } from "react-icons/fa";
 import { FaInstagram, FaXTwitter } from "react-icons/fa6";
+import atcCircularLogo from "../../assets/atc-logo-circular-white.png";
 
 function Footer() {
   return (
     <div className={styles.footerContainer}>
-      <img
-        className={styles.ATClogo}
-        src="https://thespongeclub.com/wp-content/uploads/2022/08/gary-spongebob-guide-featured.jpg"
-      ></img>
+      <img className={styles.ATClogo} src={atcCircularLogo}></img>
       <div className={styles.informationContainer}>
         {/* everything in informationContainer is organized by columns */}
         <div className={styles.informationInnerContainer}>
@@ -16,20 +14,26 @@ function Footer() {
           <div className={styles.textDiv}>
             ABOUT US
             <br />
-            <a href="https://appalachiantrail.org/">
-              {" "}
-              Appalachian Trail Conservancy
-            </a>
-            <br />
-            <a href="https://wildeast.appalachiantrail.org/our-work/about-us/contact-us/">
+            <a
+              href="https://wildeast.appalachiantrail.org/our-work/about-us/contact-us/"
+              target="_blank"
+              rel="noopener noreferrer">
               Contact Us
             </a>
           </div>
 
           <div className={styles.textDiv}>
-            GET INVOLVED
+            <a
+              href="https://appalachiantrail.org/get-involved/volunteer/"
+              target="_blank"
+              rel="noopener noreferrer">
+              GET INVOLVED
+            </a>
             <br />
-            <a href="https://volunteer.appalachiantrail.org/s/volunteer-project-search">
+            <a
+              href="https://appalachiantrail.org/waystovolunteer"
+              target="_blank"
+              rel="noopener noreferrer">
               Volunteer
             </a>
           </div>
@@ -43,7 +47,10 @@ function Footer() {
           </div>
         </div>
         <div className={styles.textDiv}>
-          <p className={styles.smallText}>These trainings are presented by the Appalachian Trail Conservancy for A.T. partners and volunteers.</p>
+          <p className={styles.smallText}>
+            These trainings are presented by the Appalachian Trail Conservancy
+            for A.T. partners and volunteers.
+          </p>
         </div>
       </div>
     </div>
