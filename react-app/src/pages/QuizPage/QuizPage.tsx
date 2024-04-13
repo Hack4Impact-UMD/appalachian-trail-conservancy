ec5a4da1a280aa211f5a155d1b3e03a6bed3ccf7
Megan Yu <yu.megan@gmail.com>
2024-04-13 11:34:49 -0400

    add pfp, button, and style - imitated styling on Trainings page - button in footer that sticks to bottom

--- react-app/src/pages/QuizPage/QuizPage.tsx
+++ react-app/src/pages/QuizPage/QuizPage.tsx
@@ -1,7 +1,30 @@
+import { Button } from "@mui/material";
+import { forestGreenButton, whiteButtonGrayBorder } from "../../muiTheme";
+import ProfileIcon from "../../components/ProfileIcon/ProfileIcon";
+import styles from "./QuizPage.module.css";
+
+const styledButtons = {
+    marginRight: "10%",
+};
+
 function QuizPage() {
     return (
-        <div>
-            <h1>Training Title - Quiz</h1>
+        <div className={styles.bodyContainer}>
+            {/* header - title & profile icon */}
+            <div className={styles.header}>
+                <h1 className={styles.nameHeading}>Training Title - Quiz</h1>
+                <ProfileIcon />
+            </div>
+
+            {/* footer */}
+            <div className={styles.footer}>
+                <Button
+                sx={{ ...forestGreenButton, ...styledButtons}}
+                variant="contained">
+                Retake quiz
+                </Button>
+            </div>
+            
         </div>
     );
 }
