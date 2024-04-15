30fef8d0dba42d7cba82001a3e2d671afd29e10b
sophtsai <sophietsai31@gmail.com>
2024-04-14 22:43:52 -0400

    Change footer link hover animation to center and add to icons

--- react-app/src/components/Footer/Footer.tsx
+++ react-app/src/components/Footer/Footer.tsx
@@ -1,6 +1,7 @@
 import styles from "./Footer.module.css";
 import { FaFacebookF, FaTiktok, FaYoutube } from "react-icons/fa";
 import { FaInstagram, FaXTwitter } from "react-icons/fa6";
+import IconButton from "@mui/material/IconButton";
 import atcCircularLogo from "../../assets/atc-logo-circular-white.png";
 
 function Footer() {
@@ -11,75 +12,76 @@ function Footer() {
         {/* everything in informationContainer is organized by columns */}
         <div className={styles.informationInnerContainer}>
           {/* everything in informationInnerContainer is organized by row */}
-          <div className={styles.textDiv}>
+          <div className={`${styles.textDiv} ${styles.hoverUnderline}`}>
             <a
               href="https://appalachiantrail.org/"
               target="_blank"
-              rel="noopener noreferrer"
-            >
+              rel="noopener noreferrer">
               <span>About Us</span>
             </a>
             <a
               href="https://wildeast.appalachiantrail.org/our-work/about-us/contact-us/"
               target="_blank"
-              rel="noopener noreferrer"
-            >
+              rel="noopener noreferrer">
               <span>Contact Us</span>
             </a>
           </div>
 
-          <div className={styles.textDiv}>
+          <div className={`${styles.textDiv} ${styles.hoverUnderline}`}>
             <a
               href="https://appalachiantrail.org/get-involved/volunteer/"
               target="_blank"
-              rel="noopener noreferrer"
-            >
+              rel="noopener noreferrer">
               <span>Get Involved</span>
             </a>
             <a
               href="https://appalachiantrail.org/waystovolunteer"
               target="_blank"
-              rel="noopener noreferrer"
-            >
+              rel="noopener noreferrer">
               <span>Volunteer</span>
             </a>
           </div>
 
-          <div className={styles.iconsDiv}>
+          <div className={`${styles.iconsDiv} ${styles.hoverUnderline}`}>
             <a
               href="https://www.facebook.com/ATHike"
               target="_blank"
-              rel="noopener noreferrer"
-            >
-              <FaFacebookF className={styles.whiteIcon} />
+              rel="noopener noreferrer">
+              <span>
+                <FaFacebookF className={styles.whiteIcon} />
+              </span>
             </a>
             <a
               href="https://www.instagram.com/appalachiantrail/"
               target="_blank"
-              rel="noopener noreferrer"
-            >
-              <FaInstagram className={styles.whiteIcon} />
+              rel="noopener noreferrer">
+              <span>
+                <FaInstagram className={styles.whiteIcon} />
+              </span>
             </a>
             <a
               href="https://www.tiktok.com/@appalachiantrail"
               target="_blank"
-              rel="noopener noreferrer"
-            >
-              <FaTiktok className={styles.whiteIcon} />
+              rel="noopener noreferrer">
+              <span>
+                <FaTiktok className={styles.whiteIcon} />
+              </span>
             </a>
             <a
               href="https://twitter.com/at_conservancy"
               target="_blank"
-              rel="noopener noreferrer"
-            >
-              <FaXTwitter className={styles.whiteIcon} />
+              rel="noopener noreferrer">
+              <span>
+                <FaXTwitter className={styles.whiteIcon} />
+              </span>
             </a>
             <a
               href="https://www.youtube.com/user/atconservancy"
               target="_blank"
-              rel="noopener noreferrer"
-            >
-              <FaYoutube className={styles.whiteIcon} />
+              rel="noopener noreferrer">
+              <span>
+                <FaYoutube className={styles.whiteIcon} />
+              </span>
             </a>
           </div>
         </div>
