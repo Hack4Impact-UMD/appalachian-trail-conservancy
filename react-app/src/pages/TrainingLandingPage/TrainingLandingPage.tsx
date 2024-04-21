327c19195b59e1bcd3e25c5fa9d6895efefe3cf7
jhoang712 <jessicahoang712@gmail.com>
2024-04-21 18:23:14 -0400

    added functionality to render buttons and opacity

--- react-app/src/pages/TrainingLandingPage/TrainingLandingPage.tsx
+++ react-app/src/pages/TrainingLandingPage/TrainingLandingPage.tsx
@@ -8,15 +8,16 @@ import Step from "@mui/material/Step";
 import StepLabel from "@mui/material/StepLabel";
 import ResourceComponent from "../../components/ResourceComponent/ResourceComponent";
 import LinearProgressWithLabel from "../../components/LinearProgressWithLabel/LinearProgressWithLabel";
-import { type Training } from '../../types/TrainingType';
-import { type VolunteerTraining } from '../../types/UserType';
+import { type Training } from "../../types/TrainingType";
+import { type VolunteerTraining } from "../../types/UserType";
 import { Button } from "@mui/material";
 import { forestGreenButton } from "../../muiTheme";
 import ProfileIcon from "../../components/ProfileIcon/ProfileIcon";
+import CompletedIcon from "../../assets/greenCircleCheck.svg";
 
 const styledButtons = {
-  marginRight: '10%',
-  marginLeft: '1%',
+  marginRight: "10%",
+  marginLeft: "1%",
 };
 
 const isCompleted = (volunteer: VolunteerTraining): boolean => {
@@ -25,48 +26,116 @@ const isCompleted = (volunteer: VolunteerTraining): boolean => {
 
 function TrainingLandingPage() {
   const volunteer: VolunteerTraining = {
-    trainingID: '',
-    progress: 'INPROGRESS',
-    dateCompleted: '',
+    trainingID: "",
+    progress: "INPROGRESS",
+    dateCompleted: "",
     numCompletedResources: 2,
-    numTotalResources: 2,
+    numTotalResources: 4,
     quizScoreRecieved: 0,
   };
 
   const training: Training = {
-    name: 'How to pet a cat',
-    description: 'blah blah blah',
-    coverImage: '',
+    name: "How to pet a cat",
+    description: "blah blah blah",
+    coverImage: "",
     resources: [
-      { type: 'VIDEO', link: 'https://example.com/video1', title: 'Video 1' },
-      { type: 'PDF', link: 'https://example.com/article1', title: 'Article 1' },
+      { type: "VIDEO", link: "https://example.com/video1", title: "Video 1" },
+      { type: "PDF", link: "https://example.com/article1", title: "Article 1" },
+      { type: "PDF", link: "https://example.com/article1", title: "Article 2" },
     ],
-    quizID: '',
+    quizID: "",
     associatedPathways: [],
-    certificationImage: '',
+    certificationImage: "",
   };
 
   const renderTrainingResources = () => {
-    return training.resources.map((resource: TrainingResource, index: number) => (
-      <div key={index}>
-        <div className={styles.trainingRow}>
-          <div className={styles.trainingInfo}>
-            <p className={styles.trainingNumber}>{index + 1}</p>
-            <p className={styles.trainingTitle}>{resource.title}</p>
-            <p className={styles.trainingType}>{resource.type}</p>
-          </div>
-          {/* Conditionally render an image if training is completed */}
-          {isCompleted(volunteer) && (
-            <div className={styles.completedIcon}>
-              <img src="/path/to/completed-icon.png" alt="Completed" className={styles.completedIconImage} />
+    return training.resources.map(
+      (resource: TrainingResource, index: number) => (
+        <div key={index}>
+          <div className={styles.trainingRow}>
+            <div
+              className={`${styles.trainingInfo} ${
+                index + 1 <= volunteer.numCompletedResources
+                  ? styles.opacityContainer
+                  : ""
+              }`}
+            >
+              <p className={styles.trainingNumber}>{index + 1}</p>
+              <p className={styles.trainingTitle}>{resource.title}</p>
+              <p className={styles.trainingType}>{resource.type}</p>
             </div>
-          )}
+            {/* Conditionally render an image if training is completed */}
+            {index + 1 <= volunteer.numCompletedResources && (
+              <img
+                className={styles.completedIcon}
+                src={CompletedIcon}
+                alt="Completed"
+              />
+            )}
+          </div>
         </div>
-      </div>
-    ));
+      )
+    );
   };
 
-  
+  const renderMarker = () => {
+    if (volunteer.numCompletedResources === 0) {
+      // Training not started
+      return (
+        <div className={`${styles.marker} ${styles.notStartedMarker}`}>
+          NOT STARTED
+        </div>
+      );
+    } else if (
+      volunteer.numCompletedResources === volunteer.numTotalResources
+    ) {
+      // Training completed
+      return (
+        <div className={`${styles.marker} ${styles.progressMarker}`}>
+          COMPLETED
+        </div>
+      );
+    }
+    // Training in progress
+    else
+      return (
+        <div className={`${styles.marker} ${styles.progressMarker}`}>
+          IN PROGRESS
+        </div>
+      );
+  };
+
+  const renderButton = () => {
+    if (volunteer.numCompletedResources === 0) {
+      return (
+        <Button
+          sx={{ ...forestGreenButton, ...styledButtons }}
+          variant="contained"
+        >
+          Start
+        </Button>
+      );
+    } else if (volunteer.quizScoreRecieved != 0) {
+      return (
+        <Button
+          sx={{ ...forestGreenButton, ...styledButtons }}
+          variant="contained"
+        >
+          Restart
+        </Button>
+      );
+    } else {
+      return (
+        <Button
+          sx={{ ...forestGreenButton, ...styledButtons }}
+          variant="contained"
+        >
+          Resume
+        </Button>
+      );
+    }
+  };
+
   return (
     <>
       <NavigationBar />
@@ -79,10 +148,15 @@ function TrainingLandingPage() {
               <ProfileIcon />
             </div>
 
-            <div className={styles.progressBarContainer}>
-              <LinearProgressWithLabel value={(volunteer.numCompletedResources / volunteer.numTotalResources) * 100} />
-              <div className={`${styles.marker} ${styles.notStartedMarker}`}>NOT STARTED</div>
-            </div>
+            <LinearProgressWithLabel
+              value={
+                (volunteer.numCompletedResources /
+                  volunteer.numTotalResources) *
+                100
+              }
+              sx={{ width: 50 }}
+            />
+            <div className={styles.progressBar}>{renderMarker()}</div>
 
             {/* ABOUT */}
             <div className={styles.container}>
@@ -96,22 +170,22 @@ function TrainingLandingPage() {
               {renderTrainingResources()}
               <div className={styles.trainingRowFinal}>
                 <div className={styles.trainingInfo}>
-                  <p className={styles.trainingNumber}>{volunteer.numCompletedResources + 1}</p>
+                  <p className={styles.trainingNumber}>
+                    {volunteer.numTotalResources}
+                  </p>
                   <p className={styles.trainingTitle}>Quiz</p>
                 </div>
-                </div>
+              </div>
             </div>
           </div>
         </div>
 
         {/* footer */}
         <div className={styles.footer}>
-          <Button sx={{ ...whiteButtonGrayBorder, }} variant="contained">
+          <Button sx={{ ...whiteButtonGrayBorder }} variant="contained">
             Back
           </Button>
-          <Button sx={{ ...forestGreenButton, ...styledButtons }} variant="contained">
-            Start
-          </Button>
+          {renderButton()}
         </div>
       </div>
     </>
