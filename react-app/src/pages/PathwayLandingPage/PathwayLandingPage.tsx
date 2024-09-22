72f48c5bef4e7ca3796fe0ff7c7696651ba94212
nick farber <nick@home>
2024-09-22 18:42:03 -0400

    Working on getting the size of the div

--- react-app/src/pages/PathwayLandingPage/PathwayLandingPage.tsx
+++ react-app/src/pages/PathwayLandingPage/PathwayLandingPage.tsx
@@ -1,15 +1,31 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import NavigationBar from "../../components/NavigationBar/NavigationBar";
 import TitleInfo from "./TitleInfo";
 import styles from "./PathwayLandingPage.module.css";
 import PathwayTile from "./PathwayTile";
-
+ 
 const PathwayLandingPage: React.FC = () => {
   const [navigationBarOpen, setNavigationBarOpen] = useState<boolean>(true);
+  const [divWidth, setDivWidth] = useState<number>();
+
+  // Can this be done better?
+  // Gets the current pathway id from url, url in form: .../pathways/<ID#>
+  const pathwayId = window.location.href.substring(window.location.href.length-20,window.location.href.length);
+
+  const div = useRef<HTMLDivElement>(null);
 
   const pathwayTitle = "Sample Pathway Title";
   const pathwayDescription = "Sample description for the pathway.";
+  const trainingNum = 5;
+  const imgSize = 55;
+  const width = divWidth;
 
+  useEffect(() => {
+    if (div.current) {
+      setDivWidth(div.current.offsetWidth);
+    }
+  }, []);
+  
   return (
     <div className={styles.pageContainer}>
       <NavigationBar open={navigationBarOpen} setOpen={setNavigationBarOpen} />
@@ -18,8 +34,8 @@ const PathwayLandingPage: React.FC = () => {
         <TitleInfo title={pathwayTitle} description={pathwayDescription} />
 
         {/* Pathway Tiles Section */}
-        <div className={styles.pathwayTiles}>
-          <PathwayTile />
+        <div className={styles.pathwayTiles} ref={div}>
+          {console.log(divWidth)}
         </div>
       </div>
     </div>
--- react-app/src/pages/PathwayLandingPage/PathwayTile.tsx
+++ react-app/src/pages/PathwayLandingPage/PathwayTile.tsx
@@ -1,5 +1,15 @@
 import React from "react";
 import styles from "./PathwayTile.module.css";
+import emptyIcon from "../../assets/PathwayTiles/Empty.svg";
+import downEndIcon from "../../assets/PathwayTiles/DownEnd.svg";
+import downLeftIcon from "../../assets/PathwayTiles/DownLeft.svg";
+import downRightIcon from "../../assets/PathwayTiles/DownRight.svg";
+import horizontalIcon from "../../assets/PathwayTiles/Horizonal.svg";
+import leftDownIcon from "../../assets/PathwayTiles/LeftDown.svg";
+import leftEndIcon from "../../assets/PathwayTiles/LeftEnd.svg";
+import rightDownIcon from "../../assets/PathwayTiles/RightDown.svg";
+import rightEndIcon from "../../assets/PathwayTiles/RightEnd.svg";
+import verticalIcon from "../../assets/PathwayTiles/Vertical.svg";
 
 const PathwayTile: React.FC = () => {
   return <div></div>;
