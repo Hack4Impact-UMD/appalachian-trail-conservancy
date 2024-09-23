017c222d493a80e66031319b16c4389d38cf9867
jhoang712 <jessicahoang712@gmail.com>
2024-09-23 00:06:05 -0400

    connected backend, added nav bar transition, added some things to PathwayTile

--- react-app/src/pages/PathwayLandingPage/PathwayLandingPage.tsx
+++ react-app/src/pages/PathwayLandingPage/PathwayLandingPage.tsx
@@ -2,43 +2,106 @@ import React, { useState, useEffect, useRef } from "react";
 import NavigationBar from "../../components/NavigationBar/NavigationBar";
 import TitleInfo from "./TitleInfo";
 import styles from "./PathwayLandingPage.module.css";
+import { useNavigate, useLocation } from "react-router-dom";
 import PathwayTile from "./PathwayTile";
- 
+import { VolunteerPathway } from "../../types/UserType";
+import { PathwayID } from "../../types/PathwayType";
+import { useAuth } from "../../auth/AuthProvider";
+import Loading from "../../components/LoadingScreen/Loading";
+
 const PathwayLandingPage: React.FC = () => {
+  const auth = useAuth();
+  const navigate = useNavigate();
+  const location = useLocation();
   const [navigationBarOpen, setNavigationBarOpen] = useState<boolean>(true);
   const [divWidth, setDivWidth] = useState<number>();
+  const [loading, setLoading] = useState<boolean>(true);
+
+  {
+    /*
+  const [volunteerPathway, setVolunteerPathway] = useState<VolunteerPathway>({
+    pathwayID: "",
+    progress: "INPROGRESS",
+    dateCompleted: "0000-00-00", // YYYY-MM-DD
+    trainingsCompleted: [], // training IDs
+    numTrainingsCompleted: 0,
+    numTotalTrainings: 0,
+  }); */
+  }
+
+  const [pathway, setPathway] = useState<PathwayID>({
+    name: "",
+    id: "",
+    shortBlurb: "",
+    description: "",
+    coverImage: "",
+    trainingIDs: [],
+    quiz: {
+      questions: [],
+      numQuestions: 0,
+      passingScore: 0,
+    },
+    badgeImage: "",
+  });
 
   // Can this be done better?
   // Gets the current pathway id from url, url in form: .../pathways/<ID#>
-  const pathwayId = window.location.href.substring(window.location.href.length-20,window.location.href.length);
+  //const pathwayId = window.location.href.substring(
+  //  window.location.href.length - 20,
+  //  window.location.href.length
+  //);
 
   const div = useRef<HTMLDivElement>(null);
 
-  const pathwayTitle = "Sample Pathway Title";
-  const pathwayDescription = "Sample description for the pathway.";
   const trainingNum = 5;
   const imgSize = 55;
   const width = divWidth;
 
   useEffect(() => {
+    // get data from nav state
+    // not retrieving volunteer pathway information yet
+    if (location.state.pathway) {
+      setPathway(location.state.pathway);
+      setLoading(false);
+    } else {
+      navigate("/pathways");
+    }
     if (div.current) {
       setDivWidth(div.current.offsetWidth);
     }
   }, []);
-  
+
   return (
-    <div className={styles.pageContainer}>
+    <>
       <NavigationBar open={navigationBarOpen} setOpen={setNavigationBarOpen} />
+      <div
+        className={`${styles.split} ${styles.right}`}
+        style={{ left: navigationBarOpen ? "250px" : "0" }}
+      >
+        {loading ? (
+          <Loading />
+        ) : (
+          <div className={styles.pageContainer}>
+            <div className={styles.content}>
+              <TitleInfo
+                title={pathway.name}
+                description={pathway.shortBlurb}
+              />
 
-      <div className={styles.content}>
-        <TitleInfo title={pathwayTitle} description={pathwayDescription} />
+              {/* Pathway Tiles Section */}
+              <div className={styles.pathwayTiles} ref={div}>
+                {/* console.log(divWidth)*/}
 
-        {/* Pathway Tiles Section */}
-        <div className={styles.pathwayTiles} ref={div}>
-          {console.log(divWidth)}
-        </div>
+                {pathway.trainingIDs.map((trainingID, index) => (
+                  /*how to pass in training ID???*/
+                  <PathwayTile tileNum={index + 1} trainingID={trainingID} />
+                ))}
+              </div>
+            </div>
+          </div>
+        )}
       </div>
-    </div>
+    </>
   );
 };
 
--- react-app/src/pages/PathwayLandingPage/PathwayTile.tsx
+++ react-app/src/pages/PathwayLandingPage/PathwayTile.tsx
@@ -4,15 +4,57 @@ import emptyIcon from "../../assets/PathwayTiles/Empty.svg";
 import downEndIcon from "../../assets/PathwayTiles/DownEnd.svg";
 import downLeftIcon from "../../assets/PathwayTiles/DownLeft.svg";
 import downRightIcon from "../../assets/PathwayTiles/DownRight.svg";
-import horizontalIcon from "../../assets/PathwayTiles/Horizonal.svg";
+import horizontalIcon from "../../assets/PathwayTiles/Horizontal.svg";
 import leftDownIcon from "../../assets/PathwayTiles/LeftDown.svg";
 import leftEndIcon from "../../assets/PathwayTiles/LeftEnd.svg";
 import rightDownIcon from "../../assets/PathwayTiles/RightDown.svg";
 import rightEndIcon from "../../assets/PathwayTiles/RightEnd.svg";
 import verticalIcon from "../../assets/PathwayTiles/Vertical.svg";
+import TrainingPopup from "../../components/TrainingPopup/TrainingPopup";
+import { useState } from "react";
+import { TrainingID } from "../../types/TrainingType";
 
-const PathwayTile: React.FC = () => {
-  return <div></div>;
+interface PathwayTileProps {
+  tileNum: number;
+  trainingID: TrainingID;
+}
+
+const PathwayTile: React.FC<PathwayTileProps> = ({ tileNum, trainingID }) => {
+  const [openTrainingPopup, setOpenTrainingPopup] = useState<boolean>(false);
+
+  return (
+    <>
+      <div
+        style={{
+          position: "relative",
+          lineHeight: 0,
+        }}
+        onClick={() => {
+          setOpenTrainingPopup(true);
+        }}
+      >
+        <img src={horizontalIcon} style={{ width: "100%" }} />
+        <div
+          style={{
+            position: "absolute",
+            left: "50%",
+            top: "50%",
+            transform: "translate(-50%, -50%)",
+            color: "white",
+            fontSize: "3rem",
+            fontWeight: "bold",
+          }}
+        >
+          {tileNum}
+        </div>
+      </div>
+      <TrainingPopup
+        open={openTrainingPopup}
+        onClose={setOpenTrainingPopup}
+        training={trainingID}
+      />
+    </>
+  );
 };
 
 export default PathwayTile;
