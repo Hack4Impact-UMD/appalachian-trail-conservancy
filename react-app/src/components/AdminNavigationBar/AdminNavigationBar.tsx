63794ff6a61702076eb0efa799456ffdc98c9483
Brandon <bnewman6@terpmail.umd.edu>
2024-10-15 16:16:19 -0400

    copied over kinda

---
+++ react-app/src/components/AdminNavigationBar/AdminNavigationBar.tsx
@@ -0,0 +1,188 @@
+import { useState } from "react";
+import { NavLink } from "react-router-dom";
+import userInactive from "../../assets/userGray.svg";
+import userActive from "../../assets/userWhite.svg";
+import atcprimarylogo from "../../assets/atc-primary-logo.png";
+import collapseArrow from "../../assets/collapseArrow.svg";
+import dashboardInactive from "../../assets/dashboardGray.svg";
+import dashboardActive from "../../assets/dashboardWhite.svg";
+import hamburger from "../../assets/hamburger.svg";
+import logout from "../../assets/logout.svg";
+import pathwaysInactive from "../../assets/pathwaysGray.svg";
+import pathwaysActive from "../../assets/pathwaysWhite.svg";
+import trainingsInactive from "../../assets/trainingsGray.svg";
+import trainingsActive from "../../assets/trainingsWhite.svg";
+import LogoutPopup from "../NavigationBar/LogoutPopup/LogoutPopup";
+import styles from "./AdminNavigationBar.module.css";
+
+interface AdminNavigationBarProps {
+  open: boolean;
+  setOpen: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const AdminNavigationBar: React.FC<AdminNavigationBarProps> = ({ open, setOpen }) => {
+  const [openLogoutPopup, setOpenLogoutPopup] = useState<boolean>(false);
+
+  const handleLogOut = (): void => {
+    setOpenLogoutPopup(true);
+  };
+
+  return (
+    <div
+      className={`${styles.navigationContainer} ${open ? "" : styles.closed} ${
+        openLogoutPopup ? styles.popupOpen : ""
+      }`}
+    >
+      {open ? (
+        <>
+          <div className={styles.logoContainer}>
+            <img src={atcprimarylogo} alt="ATC Logo" className={styles.logo} />
+          </div>
+          <div className={styles.menuItems}>
+            <div className={styles.navigationContainer}>
+              <div className={styles.arrowBox}>
+                <img
+                  src={collapseArrow}
+                  width={20}
+                  onClick={() => setOpen(false)}
+                />
+              </div>
+              <div className={styles.logoContainer}>
+                <img
+                  src={atcprimarylogo}
+                  alt="ATC Logo"
+                  className={styles.logo}
+                />
+              </div>
+              <div className={styles.menuItems}>
+                <div className={styles.tabContainer}>
+                  <NavLink
+                    to="/"
+                    className={({ isActive }) =>
+                      isActive
+                        ? `${styles.tab} ${styles.tabActive}`
+                        : `${styles.tab} ${styles.tabInActive}`
+                    }
+                  >
+                    <div>
+                      <img
+                        className={styles.iconActive}
+                        src={dashboardActive}
+                        alt="dashboard icon"
+                      />
+                      <img
+                        className={styles.iconInactive}
+                        src={dashboardInactive}
+                        alt="dashboard icon"
+                      />
+                    </div>
+                    Dashboard
+                  </NavLink>
+                </div>
+
+                <div className={styles.tabContainer}>
+                  <NavLink
+                    to="/trainings"
+                    className={({ isActive }) =>
+                      isActive
+                        ? `${styles.tab} ${styles.tabActive}`
+                        : `${styles.tab} ${styles.tabInActive}`
+                    }
+                  >
+                    <div>
+                      <img
+                        className={styles.iconActive}
+                        src={trainingsActive}
+                        alt="trainings icon"
+                      />
+                      <img
+                        className={styles.iconInactive}
+                        src={trainingsInactive}
+                        alt="trainings icon"
+                      />
+                    </div>
+                    Trainings
+                  </NavLink>
+                </div>
+
+                <div className={styles.tabContainer}>
+                  <NavLink
+                    to="/pathways"
+                    className={({ isActive }) =>
+                      isActive
+                        ? `${styles.tab} ${styles.tabActive}`
+                        : `${styles.tab} ${styles.tabInActive}`
+                    }
+                  >
+                    <div>
+                      <img
+                        className={styles.iconActive}
+                        src={pathwaysActive}
+                        alt="pathways icon"
+                      />
+                      <img
+                        className={styles.iconInactive}
+                        src={pathwaysInactive}
+                        alt="pathways icon"
+                      />
+                    </div>
+                    Pathways
+                  </NavLink>
+                </div>
+
+                <div className={styles.tabContainer}>
+                  <NavLink
+                    to="/users"
+                    className={({ isActive }) =>
+                      isActive
+                        ? `${styles.tab} ${styles.tabActive}`
+                        : `${styles.tab} ${styles.tabInActive}`
+                    }
+                  >
+                    <div>
+                      <img
+                        className={styles.iconActive}
+                        src={userActive}
+                        alt="user icon"
+                      />
+                      <img
+                        className={styles.iconInactive}
+                        src={userInactive}
+                        alt="user icon"
+                      />
+                    </div>
+                    User
+                    Management
+                  </NavLink>
+                </div>
+              </div>
+              <div className={styles.logoutContainer}>
+                <button
+                  onClick={() => {
+                    handleLogOut();
+                  }}
+                  className={styles.menuItem}
+                >
+                  <img src={logout} alt="Logout" />
+                  Log Out
+                </button>
+              </div>
+            </div>
+          </div>
+          <LogoutPopup open={openLogoutPopup} onClose={setOpenLogoutPopup} />
+        </>
+      ) : (
+        <div>
+          <img
+            src={hamburger}
+            className={styles.hamburger}
+            width={30}
+            onClick={() => setOpen(true)}
+          />
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default AdminNavigationBar;
