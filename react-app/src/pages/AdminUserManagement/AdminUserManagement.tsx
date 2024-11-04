ff05ae40db160c0d3a1ae137aaa8f05de4bacdf6
Emily Li <emilyli5@umd.edu>
2024-11-04 13:42:58 -0500

    started grid

--- react-app/src/pages/AdminUserManagement/AdminUserManagement.tsx
+++ react-app/src/pages/AdminUserManagement/AdminUserManagement.tsx
@@ -2,13 +2,25 @@ import { useState } from "react";
 import NavigationBar from "../../components/NavigationBar/NavigationBar";
 import styles from "./AdminUserManagement.module.css";
 import ProfileIcon from "../../components/ProfileIcon/ProfileIcon";
+import { IoIosSearch } from "react-icons/io";
 import ToggleButton from "@mui/material/ToggleButton";
 import ToggleButtonGroup from "@mui/material/ToggleButtonGroup";
+import { Button, InputAdornment, OutlinedInput } from "@mui/material";
+import { User } from "../../types/UserType.ts";
+import { DataGrid } from "@mui/x-data-grid";
+import {
+  forestGreenButtonPadding,
+  forestGreenButtonLarge,
+  whiteButtonGrayBorder,
+  grayBorderSearchBar,
+} from "../../muiTheme";
+import debounce from "lodash.debounce";
 import { borderColor, styled } from "@mui/system";
 
 function AdminUserManagement() {
   const [navigationBarOpen, setNavigationBarOpen] = useState<boolean>(true);
   const [alignment, setAlignment] = useState<string | null>("user");
+  const [searchQuery, setSearchQuery] = useState("");
   const handleAlignment = (
     event: React.MouseEvent<HTMLElement>,
     newAlignment: string | null
@@ -17,6 +29,32 @@ function AdminUserManagement() {
       setAlignment(newAlignment);
     }
   };
+
+  const [user1] = useState<User>({
+    auth_id: "U6ICu0t0MxOuUCygwXY7aLeJCCv2",
+    email: "sophietsai31@gmail.com",
+    firstName: "Sophie",
+    lastName: "Tsai",
+    type: "ADMIN"
+  });
+
+  const [user2] = useState<User>({
+    auth_id: "wZO7L1uS1dc9YIrq7FU6YXzZk1J3",
+    email: "h4iatctest@gmail.com",
+    firstName: "Akash",
+    lastName: "Patil",
+    type: "VOLUNTEER"
+  });
+
+  const users = [user1, user2];
+
+  const columns = [
+    { field: "auth_id", headerName: "Auth ID", width: 250 },
+    { field: "email", headerName: "Email", width: 200 },
+    { field: "firstName", headerName: "First Name", width: 150 },
+    { field: "lastName", headerName: "Last Name", width: 150 },
+    { field: "type", headerName: "User Type", width: 120 }
+  ];
 
   const CustomToggleButtonGroup = styled(ToggleButtonGroup)({
     borderRadius: "15px", // Rounded corners
@@ -55,6 +93,38 @@ function AdminUserManagement() {
     },
   });
 
+  const DataGridStyles = {
+    border: 10,
+    borderColor: "rgba(189,189,189,0.75)",
+    borderRadius: 4,
+    "& .MuiDataGrid-row:nth-child(even)": {
+      backgroundColor: "rgba(224, 224, 224, 0.75)",
+      fontFamily: `"Source-Serif 4", serif`,
+    },
+    "& .MuiDataGrid-columnHeaders": {
+      backgroundColor: "#2264E555",
+      borderRadius: 0,
+      fontFamily: `"Source-Serif 4", serif`,
+    },
+    "& .MuiDataGrid-row:nth-child(odd)": {
+      backgroundColor: "#FFFFFF",
+      fontFamily: `"Source-Serif 4", serif`,
+    },
+    "& .MuiDataGrid-footerContainer": {
+      backgroundColor: "rgba(224, 224, 224, 0.75)",
+    },
+    "& .MuiDataGrid-row:hover": {
+      backgroundColor: "#2264E535",
+      cursor: "pointer",
+    },
+  };
+
+  const updateQuery = (e: {
+    target: { value: React.SetStateAction<string> };
+  }) => setSearchQuery(e.target.value);
+
+  const debouncedOnChange = debounce(updateQuery, 200);
+
   return (
     <>
       <NavigationBar open={navigationBarOpen} setOpen={setNavigationBarOpen} />
@@ -84,6 +154,51 @@ function AdminUserManagement() {
                   PATHWAYS INFORMATION
                 </CustomToggleButton>
               </CustomToggleButtonGroup>
+            </div>
+            {/* Conditional Content Rendering */}
+            <div className={styles.contentSection}>
+              {alignment === "user" && (
+                <div>
+                  <div className={styles.searchBarContainer}>
+                    <OutlinedInput
+                      sx={grayBorderSearchBar}
+                      placeholder="Search..."
+                      onChange={debouncedOnChange}
+                      startAdornment={
+                        <InputAdornment position="start">
+                          <IoIosSearch />
+                        </InputAdornment>
+                      }
+                    />
+                  </div>
+                  <div className={styles.innerGrid}>
+                    <DataGrid
+                      rows={users}
+                      columns={columns}
+                      columnHeaderHeight={50}
+                      rowHeight={40}
+                      disableRowSelectionOnClick
+                      onRowClick={(row) => {
+                      }}
+                      sx={DataGridStyles}
+                    />
+                  </div>
+                  <h2>User Information</h2>
+                  <p>Details about user information go here.</p>
+                </div>
+              )}
+              {alignment === "training" && (
+                <div>
+                  <h2>Training Information</h2>
+                  <p>Details about training information go here.</p>
+                </div>
+              )}
+              {alignment === "pathways" && (
+                <div>
+                  <h2>Pathways Information</h2>
+                  <p>Details about pathways information go here.</p>
+                </div>
+              )}
             </div>
           </div>
         </div>
