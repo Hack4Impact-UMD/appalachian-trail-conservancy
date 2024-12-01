559133ca421bd98320a41c9704cc6b0646d78aee
Rahul-Anantuni <anantuni.rahul@gmail.com>
2024-11-30 21:05:21 -0500

    New QuillJS Textbox, Saving to DB functionality, & Toggle Tabs added

--- react-app/src/pages/AdminRegistrationManagementPage/AdminRegistrationManagementPage.tsx
+++ react-app/src/pages/AdminRegistrationManagementPage/AdminRegistrationManagementPage.tsx
@@ -7,40 +7,43 @@ import Footer from "../../components/Footer/Footer.tsx";
 import Quill from "quill";
 import "quill/dist/quill.snow.css";
 import hamburger from "../../assets/hamburger.svg";
+import { updateRegistrationEmail } from "../../backend/AdminFirestoreCalls.ts";
+import { EmailType } from "../../types/AssetsType.ts";
+import { CustomToggleButtonGroup, PurpleToggleButton } from "../../muiTheme.ts";
 
 function AdminRegistrationManagementPage() {
   const [navigationBarOpen, setNavigationBarOpen] = useState(
     !(window.innerWidth < 1200)
   );
+  const [alignment, setAlignment] = useState<string | null>("user");
+
   const [subject, setSubject] = useState("");
   const [body, setBody] = useState("");
   const [errors, setErrors] = useState({ subject: "", body: "" });
 
-  const editorContainerRef = useRef<HTMLDivElement>(null); // DOM reference for editor container
-  const quillRef = useRef<Quill | null>(null); // Quill instance reference
+  const editorContainerRef = useRef<HTMLDivElement>(null);
+  const quillRef = useRef<Quill | null>(null);
 
-  const toolbarContainer = useRef<HTMLDivElement>(null);
   const [screenWidth, setScreenWidth] = useState<number>(window.innerWidth);
 
   useEffect(() => {
     if (editorContainerRef.current && !quillRef.current) {
       console.log("Initializing Quill editor");
       quillRef.current = new Quill(editorContainerRef.current, {
+        theme: "snow",
         modules: {
-          toolbar: "#toolbar",
+          toolbar: [["bold", "italic", "underline", "link"]],
         },
       });
 
-      // Handle Quill text changes
       quillRef.current.on("text-change", () => {
         const editorContent = quillRef.current!.root.innerHTML;
         console.log("Editor content changed:", editorContent);
-        setBody(editorContent); // Store as HTML
+        setBody(editorContent);
       });
     }
-  }, []); // Empty dependency array ensures this runs only once
+  }, []);
 
-  // Update screen width on resize
   useEffect(() => {
     const handleResize = () => {
       setScreenWidth(window.innerWidth);
@@ -51,6 +54,32 @@ function AdminRegistrationManagementPage() {
       window.removeEventListener("resize", handleResize);
     };
   }, []);
+  const handleAlignment = (
+    event: React.MouseEvent<HTMLElement>,
+    newAlignment: string | null
+  ) => {
+    if (newAlignment !== null) {
+      setAlignment(newAlignment);
+    }
+  };
+  const handleSave = () => {
+    const todayDate = new Date().toISOString().split("T")[0];
+
+    const email: EmailType = {
+      subject,
+      body,
+      type: "EMAIL",
+      dateUpdated: todayDate,
+    };
+
+    updateRegistrationEmail(email)
+      .then(() => {
+        console.log("Email updated successfully!");
+      })
+      .catch((error) => {
+        console.error("Error updating email:", error);
+      });
+  };
 
   return (
     <>
@@ -62,118 +91,140 @@ function AdminRegistrationManagementPage() {
         className={`${styles.split} ${styles.right}`}
         style={{
           left: navigationBarOpen && screenWidth > 1200 ? "250px" : "0",
-        }}>
+        }}
+      >
         {!navigationBarOpen && (
           <img
             src={hamburger}
             alt="Hamburger Menu"
-            className={styles.hamburger} // Add styles to position it
+            className={styles.hamburger}
             width={30}
-            onClick={() => setNavigationBarOpen(true)} // Set sidebar open when clicked
+            onClick={() => setNavigationBarOpen(true)}
           />
         )}
         <div className={styles.outerContainer}>
           <div className={styles.content}>
             <div className={styles.header}>
-              <h1 className={styles.nameHeading}>Edit New User Email</h1>
+              <h1 className={styles.nameHeading}>Registration Management</h1>
               <ProfileIcon />
             </div>
-            <Typography
-              variant="body2"
-              style={{
-                color: "black",
-                fontWeight: "bold",
-                marginBottom: "4px",
-                marginTop: "2rem",
-              }}>
-              SUBJECT
-            </Typography>
-            <TextField
-              value={subject}
-              sx={{
-                width: "80%",
-                fontSize: "1.1rem",
-                borderRadius: "10px",
-                marginTop: "0.3rem",
-                height: "3.2rem",
-                border: "1px solid var(--blue-gray)",
-                "& fieldset": {
-                  border: "none",
-                },
-              }}
-              onChange={(e) => setSubject(e.target.value)}
-              error={Boolean(errors.subject)}
-              helperText={errors.subject}
-              fullWidth
-              margin="normal"
-            />
-
-            <div className={styles.bodyContainer}>
-              <Typography variant="body2" className={styles.bodyLabel}>
-                BODY
-              </Typography>
-
-              <div className={styles.toolbarButtons} id="toolbar">
-                <button
-                  className="ql-bold"
+            <div className={styles.buttonGroup}>
+              <CustomToggleButtonGroup
+                value={alignment}
+                exclusive
+                onChange={handleAlignment}
+              >
+                <PurpleToggleButton
+                  value="user"
+                  sx={{
+                    fontSize: "0.875rem",
+                    padding: "0.5rem 1rem",
+                    minWidth: "100px",
+                    "&.Mui-selected": {
+                      backgroundColor: "lightgray",
+                      color: "black",
+                      "&:hover": {
+                        backgroundColor: "lightgray",
+                      },
+                    },
+                  }}
+                >
+                  EDIT NEW USER EMAIL
+                </PurpleToggleButton>
+                <PurpleToggleButton
+                  value="registration"
+                  sx={{
+                    fontSize: "0.875rem",
+                    padding: "0.5rem 1rem",
+                    minWidth: "100px",
+                    "&.Mui-selected": {
+                      backgroundColor: "lightgray",
+                      color: "black",
+                      "&:hover": {
+                        backgroundColor: "lightgray",
+                      },
+                    },
+                  }}
+                >
+                  REGISTRATION CODE
+                </PurpleToggleButton>
+              </CustomToggleButtonGroup>
+            </div>
+            {alignment === "user" && (
+              <div>
+                {" "}
+                <Typography
+                  variant="body2"
                   style={{
-                    background: "none",
-                    fontSize: "1.5em",
-                    width: "2rem",
-                  }}>
-                  B
-                </button>
-                <button
-                  className="ql-italic"
+                    color: "black",
+                    fontWeight: "bold",
+                    marginBottom: "4px",
+                    marginTop: "2rem",
+                  }}
+                >
+                  SUBJECT
+                </Typography>
+                <TextField
+                  value={subject}
+                  sx={{
+                    width: "80%",
+                    fontSize: "1.1rem",
+                    borderRadius: "10px",
+                    marginTop: "0.3rem",
+                    height: "3.2rem",
+                    border: "1px solid var(--blue-gray)",
+                    "& fieldset": {
+                      border: "none",
+                    },
+                  }}
+                  onChange={(e) => setSubject(e.target.value)}
+                  error={Boolean(errors.subject)}
+                  helperText={errors.subject}
+                  fullWidth
+                  margin="normal"
+                />
+                <div className={styles.bodyContainer}>
+                  <Typography variant="body2" className={styles.bodyLabel}>
+                    BODY
+                  </Typography>
+                </div>
+                {/* Quill Editor Container */}
+                <div
                   style={{
-                    background: "none",
-                    fontSize: "1.5em",
-                    width: "2rem",
-                  }}>
-                  I
-                </button>
-                <button
-                  className="ql-underline"
-                  style={{
-                    background: "none",
-                    fontSize: "1.5em",
-                    width: "2rem",
-                  }}>
-                  U
-                </button>
+                    width: "80%",
+                    border: "1px solid black",
+                  }}
+                >
+                  <div
+                    ref={editorContainerRef}
+                    id="quillEditor"
+                    style={{
+                      height: "300px",
+                    }}
+                  ></div>
+                </div>
+                <div className={styles.buttonContainer}>
+                  <Button
+                    variant="outlined"
+                    color="secondary"
+                    className={styles.saveButton}
+                    onClick={handleSave} // Save the email when clicked
+                  >
+                    SAVE
+                  </Button>
+                  <Button
+                    variant="outlined"
+                    color="secondary"
+                    className={styles.backButton}
+                  >
+                    BACK
+                  </Button>
+                </div>
               </div>
-            </div>
-
-            {/* Quill Editor Container */}
-            <div
-              ref={editorContainerRef}
-              id="quillEditor"
-              style={{
-                height: "250px",
-                width: "80%",
-                border: "1px solid var(--blue-gray)",
-                borderRadius: "10px",
-                marginBottom: "1rem",
-                backgroundColor: "#fff",
-              }}></div>
-
-            <div className={styles.buttonContainer}>
-              <Button
-                variant="outlined"
-                color="secondary"
-                className={styles.saveButton}
-                onClick={() => console.log("Body as HTML:", body)}>
-                SAVE
-              </Button>
-              <Button
-                variant="outlined"
-                color="secondary"
-                className={styles.backButton}>
-                BACK
-              </Button>
-            </div>
+            )}
+            {alignment === "registration" && <p> Registration Code</p>}
           </div>
-        </div>
+        </div>{" "}
         <Footer />
       </div>
     </>
