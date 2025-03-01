2febac6c3420e6bd805d60955708222e92ae287a
Sophie Tsai <sophietsai31@gmail.com>
2025-03-01 16:50:50 -0500

    Update footer styling

--- react-app/src/components/Footer/Footer.tsx
+++ react-app/src/components/Footer/Footer.tsx
@@ -22,94 +22,89 @@ function Footer() {
     <div className={styles.footerContainer}>
       {screenWidth > 450 ? (
         <div className={styles.content}>
-          {/* <div style={{ display: "flex", flexDirection: "column" }}>
-            <div
-              style={{
-                display: "flex",
-                flexDirection: "row",
-                alignContent: "center",
-                justifyContent: "space-evenly",
-              }}> */}
-          <img className={styles.ATClogo} src={atcCircularLogo} />
-          <div className={styles.informationContainer}>
-            {/* everything in informationContainer is organized by columns */}
-            <div className={styles.informationInnerContainer}>
-              {/* everything in informationInnerContainer is organized by row */}
-              <div className={`${styles.textDiv} ${styles.hoverUnderline}`}>
-                <a
-                  href="https://appalachiantrail.org/"
-                  target="_blank"
-                  rel="noopener noreferrer">
-                  <span>About Us</span>
-                </a>
-                <a
-                  href="https://wildeast.appalachiantrail.org/our-work/about-us/contact-us/"
-                  target="_blank"
-                  rel="noopener noreferrer">
-                  <span>Contact Us</span>
-                </a>
-              </div>
+          <div className={styles.column}>
+            <div className={styles.topRow}>
+              <img className={styles.ATClogo} src={atcCircularLogo} />
+              <div className={styles.informationContainer}>
+                {/* everything in informationContainer is organized by columns */}
+                <div className={styles.informationInnerContainer}>
+                  {/* everything in informationInnerContainer is organized by row */}
+                  <div className={`${styles.textDiv} ${styles.hoverUnderline}`}>
+                    <a
+                      href="https://appalachiantrail.org/"
+                      target="_blank"
+                      rel="noopener noreferrer">
+                      <span>About Us</span>
+                    </a>
+                    <a
+                      href="https://wildeast.appalachiantrail.org/our-work/about-us/contact-us/"
+                      target="_blank"
+                      rel="noopener noreferrer">
+                      <span>Contact Us</span>
+                    </a>
+                  </div>
 
-              <div className={`${styles.textDiv} ${styles.hoverUnderline}`}>
-                <a
-                  href="https://appalachiantrail.org/get-involved/volunteer/"
-                  target="_blank"
-                  rel="noopener noreferrer">
-                  <span>Get Involved</span>
-                </a>
-                <a
-                  href="https://appalachiantrail.org/waystovolunteer"
-                  target="_blank"
-                  rel="noopener noreferrer">
-                  <span>Volunteer</span>
-                </a>
-              </div>
+                  <div className={`${styles.textDiv} ${styles.hoverUnderline}`}>
+                    <a
+                      href="https://appalachiantrail.org/get-involved/volunteer/"
+                      target="_blank"
+                      rel="noopener noreferrer">
+                      <span>Get Involved</span>
+                    </a>
+                    <a
+                      href="https://appalachiantrail.org/waystovolunteer"
+                      target="_blank"
+                      rel="noopener noreferrer">
+                      <span>Volunteer</span>
+                    </a>
+                  </div>
 
-              <div className={`${styles.iconsDiv} ${styles.hoverUnderline}`}>
-                <a
-                  href="https://www.facebook.com/ATHike"
-                  target="_blank"
-                  rel="noopener noreferrer">
-                  <span>
-                    <FaFacebookF className={styles.whiteIcon} />
-                  </span>
-                </a>
-                <a
-                  href="https://www.instagram.com/appalachiantrail/"
-                  target="_blank"
-                  rel="noopener noreferrer">
-                  <span>
-                    <FaInstagram className={styles.whiteIcon} />
-                  </span>
-                </a>
-                <a
-                  href="https://www.tiktok.com/@appalachiantrail"
-                  target="_blank"
-                  rel="noopener noreferrer">
-                  <span>
-                    <FaTiktok className={styles.whiteIcon} />
-                  </span>
-                </a>
-                <a
-                  href="https://twitter.com/at_conservancy"
-                  target="_blank"
-                  rel="noopener noreferrer">
-                  <span>
-                    <FaXTwitter className={styles.whiteIcon} />
-                  </span>
-                </a>
-                <a
-                  href="https://www.youtube.com/user/atconservancy"
-                  target="_blank"
-                  rel="noopener noreferrer">
-                  <span>
-                    <FaYoutube className={styles.whiteIcon} />
-                  </span>
-                </a>
+                  <div
+                    className={`${styles.iconsDiv} ${styles.hoverUnderline}`}>
+                    <a
+                      href="https://www.facebook.com/ATHike"
+                      target="_blank"
+                      rel="noopener noreferrer">
+                      <span>
+                        <FaFacebookF className={styles.whiteIcon} />
+                      </span>
+                    </a>
+                    <a
+                      href="https://www.instagram.com/appalachiantrail/"
+                      target="_blank"
+                      rel="noopener noreferrer">
+                      <span>
+                        <FaInstagram className={styles.whiteIcon} />
+                      </span>
+                    </a>
+                    <a
+                      href="https://www.tiktok.com/@appalachiantrail"
+                      target="_blank"
+                      rel="noopener noreferrer">
+                      <span>
+                        <FaTiktok className={styles.whiteIcon} />
+                      </span>
+                    </a>
+                    <a
+                      href="https://twitter.com/at_conservancy"
+                      target="_blank"
+                      rel="noopener noreferrer">
+                      <span>
+                        <FaXTwitter className={styles.whiteIcon} />
+                      </span>
+                    </a>
+                    <a
+                      href="https://www.youtube.com/user/atconservancy"
+                      target="_blank"
+                      rel="noopener noreferrer">
+                      <span>
+                        <FaYoutube className={styles.whiteIcon} />
+                      </span>
+                    </a>
+                  </div>
+                </div>
               </div>
             </div>
-            {/* </div> */}
-            {/* </div> */}
             <div className={styles.statement}>
               These trainings are presented by the Appalachian Trail Conservancy
               for A.T. partners and volunteers.
@@ -118,8 +113,8 @@ function Footer() {
         </div>
       ) : (
         <div className={styles.content}>
+          <img className={styles.ATClogo} src={atcCircularLogo} />
           <div className={styles.topContainer}>
-            <img className={styles.ATClogo} src={atcCircularLogo} />
             <div className={`${styles.textDiv} ${styles.hoverUnderline}`}>
               <a
                 href="https://appalachiantrail.org/"
