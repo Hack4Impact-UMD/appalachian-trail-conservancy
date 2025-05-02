0af6d33be6aca546c5c72c2b05926c59e56d9a77
Akash Patil <110560351+akashpatil0@users.noreply.github.com>
2025-05-02 15:05:01 -0400

    edited email cloud functions + changed to atc email. edited email confirmation

--- react-app/src/pages/LoginPage/VolunteerLoginPage/VolunteerLoginPage.tsx
+++ react-app/src/pages/LoginPage/VolunteerLoginPage/VolunteerLoginPage.tsx
@@ -22,7 +22,7 @@ function VolunteerLoginPage() {
   const [loading, setLoading] = useState<boolean>(true);
   const [failureMessage, setFailureMessage] = useState<string>("");
   const [email, setEmail] = useState<string>("");
-  const [viewConfirmation, setViewConfirmation] = useState<boolean>(false);
+  const [viewConfirmation, setViewConfirmation] = useState<boolean>(true);
   const [displayText, setDisplayText] = useState<string>("");
   const [showLoading, setShowLoading] = useState<boolean>(false);
 
@@ -140,6 +140,9 @@ function VolunteerLoginPage() {
           <span className={styles.greenText}>{displayText}</span>.
         </p>
         <p>Please use the link to access your dashboard.</p>
+        <p className={styles.checkSpamMessage}>
+          If you don't see our email, please check your spam folder.
+        </p>
       </div>
     </div>
   );
